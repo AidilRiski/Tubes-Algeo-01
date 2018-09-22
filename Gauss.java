4d043b8a9daa250816c6091e7b273185929e1c68
AidilRiski <riskisultan@yahoo.com>
2018-09-22 14:21:13 +0700

    Echelon finished.

--- Gauss.java
+++ Gauss.java
@@ -1,5 +1,19 @@
 class Gauss{
     public static void main(String[] args){
         Matriks M1 = new Matriks(3, 3);
+        M1.BacaMatriks();
+        CreateEchelon(M1);
+        M1.TulisMatriks();
+    }
+    static void CreateEchelon(Matriks M){
+        for (int i = M.IdxBrsMin; i < M.NBrsEff; i++){
+            for (int j = i + 1; j <= M.NBrsEff; j++){
+                Double multiplier = M.Mem[j][i];
+                Double divider = M.Mem[i][i];
+                for (int k = i;  k <= M.NKolEff; k++){
+                    M.Mem[j][k] -= multiplier * M.Mem[i][k] / divider;
+                }
+            }
+        }
     }
 }
--- Matriks.java
+++ Matriks.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Matriks{
     public Double[][] Mem = new Double[100][100];
     public int NBrsEff;
@@ -8,12 +10,29 @@ class Matriks{
     Matriks(int BrsEff, int KolEff){
         this.NBrsEff = BrsEff;
         this.NKolEff = KolEff;
+        for (int i = this.IdxBrsMin; i <= this.NBrsEff; i++){
+            for (int j = this.IdxKolMin; j <= this.NKolEff; j++){
+                this.Mem[i][j] = 0.0;
+            }
+        }
     }
-
-    void CreateEchelon(){
-        for (int i = this.IdxBrsMin; i < this.NBrsEff; i++){
-            for (int j = i + 1; j <= this.NBrsEff; j++){
-                
+    void BacaMatriks(){
+        Scanner input = new Scanner(System.in);
+        for (int i = this.IdxBrsMin; i <= this.NBrsEff; i++){
+            for (int j = this.IdxKolMin; j <= this.NKolEff; j++){
+                this.Mem[i][j] = input.nextDouble();
+            }
+        }
+    }
+    void TulisMatriks(){
+        for (int i = this.IdxBrsMin; i <= this.NBrsEff; i++){
+            for (int j = this.IdxKolMin; j <= this.NKolEff; j++){
+                System.out.print(this.Mem[i][j]);
+                if (j < this.NKolEff){
+                    System.out.print(" ");
+                }else if (j == this.NKolEff){
+                    System.out.println();
+                }
             }
         }
     }
