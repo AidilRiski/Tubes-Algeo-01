8b8e1576a0c36f07014ec7fbb06806b94382b65f
johncenapiuw <edward.jaya31@gmail.com>
2018-09-27 18:16:09 +0700

    BACAFILEINTERPOLASI DAN DRIVER MATRIKS SEBAGIAN

--- FileEksternal.java
+++ FileEksternal.java
@@ -1,9 +1,20 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.*;
+import java.text.DecimalFormat;
+import java.math.RoundingMode;
 
 class FileEksternal{
-  public File file = new File ("aingcupu.txt");
+ public File file;
+
+
+  void AssignNamaFile (String X) throws FileNotFoundException, IOException  {
+    this.file = new File (X);
+    //this.reader = new BufferedReader(new FileReader(x));
+  }
+
   int GetNElmtFileSPL () {
     int count = 0;
     Scanner scan;
@@ -11,6 +22,8 @@ class FileEksternal{
       scan = new Scanner (this.file);
       while (scan.hasNextDouble()){
         count = count + 1;
+        //System.out.println(count);
+        scan.next();
       }
     } catch (FileNotFoundException e1){
         e1.printStackTrace();
@@ -18,29 +31,71 @@ class FileEksternal{
     return count;
   }
 
-  int GetNKolEff () {
-    int count = 0;
-    Scanner scan;
+  int GetNBrsFileSPL (String X) throws FileNotFoundException, IOException  {
+    int countBrs = 0;
+    BufferedReader reader;
     try {
-      scan = new Scanner (this.file);
-      while (scan.hasNextDouble()){
-        count = count + 1;
+      reader = new BufferedReader(new FileReader (X));
+      while (reader.readLine() != null){
+        countBrs++;
+        //System.out.println(countBrs);
       }
     } catch (FileNotFoundException e1){
         e1.printStackTrace();
     }
-    return count;
+    return countBrs;
   }
 
-  void BacaFileSPL () {
+  int GetNKolomFileSPL(String X) throws FileNotFoundException, IOException{
+    int countKolom = GetNElmtFileSPL() / GetNBrsFileSPL(X);
+    //System.out.println(countKolom);
+    return countKolom;
+  }
+
+  void BacaMatriksFileSPL (Matriks M, String X) throws FileNotFoundException, IOException{
+    M.MakeEmpty (GetNBrsFileSPL(X) , GetNKolomFileSPL(X));
     Scanner scan;
+    int i,j;
     try {
       scan = new Scanner (this.file);
       while (scan.hasNextDouble()){
-        System.out.println(scan.nextDouble() );
+        for (i = M.IdxBrsMin ; i<= M.NBrsEff ; i++) {
+          for (j = M.IdxKolMin ; j <= M.NKolEff ; j++) {
+            M.Mem[i][j] = scan.nextDouble();
+          }
+        }
       }
     } catch (FileNotFoundException e1){
         e1.printStackTrace();
     }
   }
+
+  void BacaMatriksFileInterpolasi (Matriks M, String X) throws FileNotFoundException, IOException{
+    M.MakeEmpty (GetNBrsFileSPL(X) , GetNBrsFileSPL(X)+1);
+    Scanner scan;
+    int i,j;
+    double temp;
+    double bulat;
+    try {
+      scan = new Scanner (this.file);
+      while (scan.hasNextDouble()){
+        for (i = M.IdxBrsMin ; i<= M.NBrsEff ; i++) {
+          temp = scan.nextDouble();
+          for (j = M.IdxKolMin ; j <= M.NKolEff ; j++) {
+            if (j != M.NKolEff) {
+              DecimalFormat df = new DecimalFormat("#.##########");
+              df.setRoundingMode(RoundingMode.CEILING);
+              bulat = (Math.pow(temp,j-1));
+              M.Mem[i][j] = df.format (bulat);  
+            }
+            else
+              M.Mem[i][j] = scan.nextDouble();
+          }
+        }
+      }
+    } catch (FileNotFoundException e1){
+        e1.printStackTrace();
+    }
+  }
+
 }
--- TugasBesarAlgeo.java
+++ TugasBesarAlgeo.java
@@ -1,15 +1,235 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.*;
+import java.text.DecimalFormat;
+import java.math.RoundingMode;
 
 class TugasBesarAlgeo{
-    public static void main(String[] args){
-        Matriks M1 = new Matriks(3, 4);
-        M1.BacaMatriks(3, 4);
-        Gauss G1 = new Gauss(M1);
-        G1.CreateEchelon();
-        G1.matriks.TulisMatriks();
-        G1.CreateReducedEchelon();
-        G1.matriks.TulisMatriks();
-    }    
+
+
+    public static void TulisMenu () {
+      System.out.println ("Silakan pilih salah satu menu dibawah berikut!");
+      System.out.println ("1. Eliminasi Gauss.");
+      System.out.println ("2. Eliminasi Gauss-Jordan");
+      System.out.println ("3. Keluar Program ");
+      System.out.print ("Ketik '1' atau '2' atau '3' pada keyboard: ");
+    }
+
+    public static void TulisMenuBaca () {
+      System.out.println ("Silakan pilih salah satu menu dibawah berikut!");
+      System.out.println ("1. Baca dari keyboard");
+      System.out.println ("2. Baca dari file");
+      System.out.println ("3. Keluar Program ");
+      System.out.print ("Ketik '1' atau '2' atau '3' pada keyboard: ");
+    }
+
+  /*  public static void MenuMatriksEselon () {
+      System.out.print ("Masukkan jumlah baris efektif: ");
+      Brs = masuk.nextInt();
+      System.out.print ("Masukkan jumlah kolom efektif: ");
+      Kol = masuk.nextInt();
+      M1.MakeEmpty(Brs,Kol);
+      System.out.println ("Masukkan elemen matriks:");
+      M1.BacaMatriks(Brs,Kol);
+      Gauss G1 = new Gauss(M1);
+      G1.CreateEchelon();
+      System.out.println ("Matriks eselon: ");
+      G1.matriks.TulisMatriks();
+    } */
+
+    public static void main(String[] args)throws FileNotFoundException, IOException {
+      String namafile = new String();
+      int menumunculpertama, menumunculkedua, menumunculketiga;
+      int Brs, Kol;
+      FileEksternal f = new FileEksternal();
+      Matriks M1 = new Matriks (1,1);
+      /*System.out.println ("Masukkan nama FileEksternal dengan format .txt (tulis .txt juga)");
+      System.out.println ("(Contoh : aingcupu.txt)");
+      System.out.print ("Nama file : ");
+      Scanner sc = new Scanner(System.in);
+      namafile = sc.nextLine();
+      f.AssignNamaFile(namafile);
+      f.BacaMatriksFileSPL(M1, namafile);
+      */
+
+      System.out.println ("----------------------------------------");
+      System.out.println ("SELAMAT DATANG DI TUGAS BESAR ALJABAR GEOMETRI!");
+      System.out.println ("----------------------------------------");
+      System.out.println ();
+
+      menumunculpertama = 0;
+
+      while (menumunculpertama != 3){
+      System.out.println ("Silakan pilih salah satu menu dibawah berikut!");
+      System.out.println ("1. Sistem Persamaan Linier");
+      System.out.println ("2. Interpolasi Polinom");
+      System.out.println ("3. Keluar Program");
+      System.out.print ("Ketik '1' atau '2' atau '3' pada keyboard: ");
+
+      Scanner masuk = new Scanner (System.in);
+      menumunculpertama = masuk.nextInt();
+
+      while ((menumunculpertama != 1 ) && (menumunculpertama != 2) && (menumunculpertama != 3)) {
+        System.out.print ("Silakan masukkan input yang benar: ");
+        menumunculpertama = masuk.nextInt();
+      }
+
+        if (menumunculpertama == 1) {
+          TulisMenu();
+          menumunculkedua = masuk.nextInt();
+          while ((menumunculkedua != 1 ) && (menumunculkedua != 2) && (menumunculkedua != 3)) {
+            System.out.print ("Silakan masukkan input yang benar: ");
+            menumunculkedua = masuk.nextInt();
+          }
+
+          if (menumunculkedua == 1) {
+              TulisMenuBaca();
+              menumunculketiga = masuk.nextInt();
+
+              while ((menumunculketiga != 1 ) && (menumunculketiga != 2) && (menumunculketiga != 3)) {
+                System.out.print ("Silakan masukkan input yang benar: ");
+                menumunculketiga = masuk.nextInt();
+              }
+
+              if (menumunculketiga == 1) {
+                System.out.print ("Masukkan jumlah baris efektif: ");
+                Brs = masuk.nextInt();
+                System.out.print ("Masukkan jumlah kolom efektif: ");
+                Kol = masuk.nextInt();
+                M1.MakeEmpty(Brs,Kol);
+                System.out.println ("Masukkan elemen matriks:");
+                M1.BacaMatriks(Brs,Kol);
+                Gauss G1 = new Gauss(M1);
+                G1.CreateEchelon();
+                System.out.println ("Matriks eselon: ");
+                G1.matriks.TulisMatriks();
+
+              } else if (menumunculketiga == 2) {
+                System.out.println ("Masukkan nama FileEksternal dengan format .txt (tulis .txt juga)");
+                System.out.println ("(Contoh : aingcupu.txt)");
+                System.out.print ("Nama file : ");
+                Scanner sc = new Scanner(System.in);
+                namafile = sc.nextLine();
+                f.AssignNamaFile(namafile);
+                f.BacaMatriksFileSPL (M1,namafile);
+                System.out.println();
+                System.out.println ("Matriks yang Anda masukkan adalah:");
+                M1.TulisMatriks();
+                Gauss G1 = new Gauss(M1);
+                G1.CreateEchelon();
+                System.out.println ("Matriks eselon: ");
+                G1.matriks.TulisMatriks();
+
+            } else if (menumunculketiga == 3) {
+              break;
+            }
+
+          } else if (menumunculkedua == 2) {
+
+            TulisMenuBaca();
+            menumunculketiga = masuk.nextInt();
+
+            while ((menumunculketiga != 1 ) && (menumunculketiga != 2) && (menumunculketiga != 3)) {
+              System.out.print ("Silakan masukkan input yang benar: ");
+              menumunculketiga = masuk.nextInt();
+            }
+
+            if (menumunculketiga == 1) {
+              System.out.print ("Masukkan jumlah baris efektif: ");
+              Brs = masuk.nextInt();
+              System.out.print ("Masukkan jumlah kolom efektif: ");
+              Kol = masuk.nextInt();
+              M1.MakeEmpty(Brs,Kol);
+              System.out.println ("Masukkan elemen matriks:");
+              M1.BacaMatriks(Brs,Kol);
+              Gauss G1 = new Gauss(M1);
+              G1.CreateReducedEchelon();
+              System.out.println ("Matriks eselon tereduksi : ");
+              G1.matriks.TulisMatriks();
+
+            } else if (menumunculketiga == 2) {
+              System.out.println ("Masukkan nama FileEksternal dengan format .txt (tulis .txt juga)");
+              System.out.println ("(Contoh : aingcupu.txt)");
+              System.out.print ("Nama file : ");
+              Scanner sc = new Scanner(System.in);
+              namafile = sc.nextLine();
+              f.AssignNamaFile(namafile);
+              f.BacaMatriksFileSPL (M1,namafile);
+              System.out.println();
+              System.out.println ("Matriks yang Anda masukkan adalah:");
+              M1.TulisMatriks();
+              Gauss G1 = new Gauss(M1);
+              G1.CreateReducedEchelon();
+              System.out.println ("Matriks eselon tereduksi: ");
+              G1.matriks.TulisMatriks();
+
+          } else if (menumunculketiga == 3) {
+            break;
+          }
+
+        } else if (menumunculkedua == 3) {
+          break;
+        }
+
+    } else if (menumunculpertama == 2) { // INTERPOLASI
+      TulisMenu();
+      menumunculkedua = masuk.nextInt();
+      while ((menumunculkedua != 1 ) && (menumunculkedua != 2) && (menumunculkedua != 3)) {
+        System.out.print ("Silakan masukkan input yang benar: ");
+        menumunculkedua = masuk.nextInt();
+      }
+
+      if (menumunculkedua == 1) {
+          TulisMenuBaca();
+          menumunculketiga = masuk.nextInt();
+
+          while ((menumunculketiga != 1 ) && (menumunculketiga != 2) && (menumunculketiga != 3)) {
+            System.out.print ("Silakan masukkan input yang benar: ");
+            menumunculketiga = masuk.nextInt();
+          }
+
+          if (menumunculketiga == 1) {
+            System.out.print ("Masukkan jumlah baris efektif: ");
+            Brs = masuk.nextInt();
+            System.out.print ("Masukkan jumlah kolom efektif: ");
+            Kol = masuk.nextInt();
+            M1.MakeEmpty(Brs,Kol);
+            System.out.println ("Masukkan elemen matriks:");
+            M1.BacaMatriks(Brs,Kol);
+            Gauss G1 = new Gauss(M1);
+            G1.CreateEchelon();
+            System.out.println ("Matriks eselon: ");
+            G1.matriks.TulisMatriks();
+
+          } else if (menumunculketiga == 2) {
+            System.out.println ("Masukkan nama FileEksternal dengan format .txt (tulis .txt juga)");
+            System.out.println ("(Contoh : aingcupu.txt)");
+            System.out.print ("Nama file : ");
+            Scanner sc = new Scanner(System.in);
+            namafile = sc.nextLine();
+            f.AssignNamaFile(namafile);
+            f.BacaMatriksFileInterpolasi (M1,namafile);
+            System.out.println();
+            System.out.println ("Matriks yang Anda masukkan adalah:");
+            M1.TulisMatriks();
+            Gauss G1 = new Gauss(M1);
+            G1.CreateEchelon();
+            System.out.println ("Matriks eselon: ");
+            G1.matriks.TulisMatriks();
+
+        } else if (menumunculketiga == 3) {
+          break;
+        }
+      } else if (menumunculkedua == 2) {
+
+      }
+
+
+    } else if (menumunculpertama == 3) {
+      break;
+    }
+  }
+}
 }
